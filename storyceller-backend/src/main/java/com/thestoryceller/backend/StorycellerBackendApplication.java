6d246ee38d3716016e6c9c1a37fa4013196f8c7c
rachitsharma300 <rachitsharma300@gmail.com>
2026-06-11 22:32:06 +0530

    feat: initialize backend with Maven wrapper, application properties, and basic structure

---
+++ storyceller-backend/src/main/java/com/thestoryceller/backend/StorycellerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.thestoryceller.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorycellerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorycellerBackendApplication.class, args);
+	}
+
+}
